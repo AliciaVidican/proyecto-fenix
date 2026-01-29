e17f9643afc13916af2efbb6695037826407bbfa
Alicia Damaris Vidican <aliciavidican13@gmail.com>
2026-01-29 09:45:28 +0100

    Añadidas pruebas de refactorización

--- proyecto-fenix/src/test/java/com/fenix/ProcesadorUsuariosTest.java
+++ proyecto-fenix/src/test/java/com/fenix/ProcesadorUsuariosTest.java
@@ -1,14 +1,15 @@
 package com.fenix;
 
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 import java.util.List;
 
-public class EjemploPrueba {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class ProcesadorUsuariosTest {
 
     // Ejemplo de prueba
     @Test
-    void testProcesarListaComportamientoActual() {
+    public void testProcesarListaComportamientoActual() {
         ProcesadorUsuarios procesador = new ProcesadorUsuarios();
         List<String> lista = List.of("Ana:1", "Luis:2", "Eva:1", "Juan:99");
         String resultado = procesador.procesarLista(lista);
